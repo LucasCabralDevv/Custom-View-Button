c3b7268512d5e930cf3ff2cc5a6a9992e3e3152f
Lucas Cabral <lucas_cabral.deve@outlook.com>
2021-06-14 16:10:04 -0300

    ProgressButton customView finalizada.

--- app/src/main/java/com/lucascabral/customviewprogressbutton/MainActivity.kt
+++ app/src/main/java/com/lucascabral/customviewprogressbutton/MainActivity.kt
@@ -2,10 +2,23 @@ package com.lucascabral.customviewprogressbutton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import com.lucascabral.customviewprogressbutton.databinding.ActivityMainBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.mainProgressButton.setOnClickListener {
+            binding.mainProgressButton.setLoading()
+            lifecycleScope.launch {
+                delay(3_000)
+                binding.mainProgressButton.setNormal()
+            }
+        }
     }
 }
